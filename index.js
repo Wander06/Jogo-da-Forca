8a86e36bc2a90ff2c9692398de9e0333f5f69f7d
Wander06 <wandersonpsilva7@gmail.com>
2023-07-21 13:20:35 -0300

    criando-logica-de-escolha-palavras

---
+++ index.js
@@ -0,0 +1,20 @@
+var palavras = {
+    frutas: {
+        lista: ["Limão", "Laranja", "Tomate", "Uva", "Melancia", "Fruta-Pão"],
+        dica: "A dica é uma fruta!"
+    },
+
+    carros: {
+        lista: ["Corsel", "Corolla", "Fusca", "Chevette"],
+        dica: "A dica é um tipo de carro!"
+    }
+}
+
+
+const escolherPalavras = (obj) => {
+    for(var i = 0; i < obj.length; i++){
+
+    }
+}
+
+console.log(palavras.frutas.lista.length)
