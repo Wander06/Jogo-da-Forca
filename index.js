da0f3983e5751ee49b3447ff51e6453c6f822b02
Wander06 <wandersonpsilva7@gmail.com>
2023-07-27 20:52:34 -0300

    logica-escolher-de-palavra

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var palavras = {
+const palavras = {
     frutas: {
         lista: ["Limão", "Laranja", "Tomate", "Uva", "Melancia", "Fruta-Pão"],
         dica: "A dica é uma fruta!"
@@ -12,13 +12,35 @@ var palavras = {
 
 
 const escolherPalavras = (obj) => {
-    var categorias = []
+    var categorias = [];
+    var indiceCategoria = 0;
+    var indiceLista = 0;
+    var lista;
+    var palavraAleatoria = "";
+    var dica = "";
 
-    for(var chave in obj){
-        console.log(chave)
-        categorias.push(chave)
+    // Percorre o objeto e adiciona todas as propriedades em formato de string, na array categorias 
+    for (var prop in obj) {
+        categorias.push(prop)
     }
-    console.log(obj[categorias[0]])
+
+    // Sorteia um número de 0 ate o tamanho de itens na array categorias 
+    indiceCategoria = Math.floor(Math.random() * categorias.length)
+
+    // Seleciona a array de palavras dentro das propriedades do objeto de acordo com a categoria sorteada pelo indiceCategoria e armazena na var lista
+    lista = obj[categorias[indiceCategoria]]["lista"];
+
+    // Seleciona a propriedade dica dentro da propriedade do objeto de acordo com o indice sorteado pelo indiceCategoria na var dica
+    dica = obj[categorias[indiceCategoria]]["dica"];
+
+    // Sorteia um número de 0 até comprimento da variavel lista
+    indiceLista = Math.floor(Math.random() * lista.length)
+
+    // E finalmente sorteia a palavra aleatoria usando um indice aleatorio na var lista
+    palavraAleatoria = lista[indiceLista]
+
+    console.log(palavraAleatoria)
+    console.log(dica)
 }
 
 escolherPalavras(palavras)
