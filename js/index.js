afc39316818c9e010d77d0905ada72636160338a
Wander06 <wandersonpsilva7@gmail.com>
2023-08-03 17:15:11 -0300

    ocultar-palavras-add

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import palavras from "./palavras.js";
 
 var palavraAleatoria = "";
 var dica = "";
+var palavraOcultada = "";
 
 const escolherPalavras = (obj) => {
     var categorias = [];
@@ -11,11 +12,11 @@ const escolherPalavras = (obj) => {
 
     // Percorre o objeto e adiciona todas as propriedades em formato de string, na array categorias 
     for (var prop in obj) {
-        categorias.push(prop)
+        categorias.push(prop);
     }
 
     // Sorteia um número de 0 ate o tamanho de itens na array categorias 
-    indiceCategoria = Math.floor(Math.random() * categorias.length)
+    indiceCategoria = Math.floor(Math.random() * categorias.length);
 
     // Seleciona a array de palavras dentro das propriedades do objeto de acordo com a categoria sorteada pelo indiceCategoria e armazena na var lista
     lista = obj[categorias[indiceCategoria]]["lista"];
@@ -24,13 +25,33 @@ const escolherPalavras = (obj) => {
     dica = obj[categorias[indiceCategoria]]["dica"];
 
     // Sorteia um número de 0 até comprimento da variavel lista
-    indiceLista = Math.floor(Math.random() * lista.length)
+    indiceLista = Math.floor(Math.random() * lista.length);
 
     // E finalmente sorteia a palavra aleatoria usando um indice aleatorio na var lista
-    palavraAleatoria = lista[indiceLista]
+    palavraAleatoria = lista[indiceLista];
 
-    console.log(palavraAleatoria)
-    console.log(dica)
+    console.log(palavraAleatoria);
+    console.log(dica);
 }
 
-escolherPalavras(palavras)
+const ocultarPalavra = () =>{
+    if(palavraOcultada !== ""){
+        palavraOcultada = ""
+    }
+    for(var i = 0; i < palavraAleatoria.length; i++){
+        palavraOcultada += "-";
+    }
+    console.log(palavraOcultada);
+}
+
+const jogo = () => {
+    escolherPalavras(palavras);
+    ocultarPalavra();
+    var textoPalavra = document.querySelector(".palavraOculta");
+    var textoDica = document.querySelector(".dica");
+
+    textoPalavra.innerHTML = palavraOcultada;
+    textoDica.innerHTML = dica;
+}
+
+document.querySelector(".comecar").addEventListener("click", jogo);
